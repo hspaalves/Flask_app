aab8c974df73c12e84932d7f9689ea735be2288e
Anderson Alves <andersonalves4608@gmail.com>
2019-09-06 14:25:54 -0300

    Inserindo String de conexão

--- InternetLibrary/config.py
+++ InternetLibrary/config.py
@@ -4,13 +4,13 @@ import os
 class Development(object):
     DEBUG = True
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgres://0.0.0.0:5432/Borges'
 
 
 class Production(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgres://0.0.0.0:5432/Borges'
 
 
 app_config = {
