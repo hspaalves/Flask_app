436f7b4d91e2113d2d84da707ed175487eacedfb
Anderson Alves <andersonalves4608@gmail.com>
2019-09-06 14:27:34 -0300

    Importando config, alterando mensagem ao iniciar a aplicação

--- InternetLibrary/app.py
+++ InternetLibrary/app.py
@@ -1,18 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api
-from flask_cors import CORS
-
-app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-api = Api(app)
+from config import app_config
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-
-api.add_resource(HelloWorld, '/')
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+def create_app(env_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[env_name])
+    @app.route('/', methods=['GET'])
+    def index():
+        return 'Congratulations! Your first endpoint is workin'
+    return app
